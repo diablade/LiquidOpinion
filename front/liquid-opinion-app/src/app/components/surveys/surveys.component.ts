beb4cf19bcf944288be9c0a5c88077b21a8b6273
niko <diablade@hotmail.com>
2020-12-14 16:49:02 +0100

    ui survey test3

--- front/liquid-opinion-app/src/app/components/surveys/surveys.component.ts
+++ front/liquid-opinion-app/src/app/components/surveys/surveys.component.ts
@@ -3,6 +3,7 @@ import {SurveyService} from '../../services/survey.service';
 import {Image, Member, Survey} from '../../models/survey';
 import {Subscription} from 'rxjs';
 import {faShareAlt, faHeart} from '@fortawesome/free-solid-svg-icons';
+
 // import * as moment from 'moment';
 
 @Component({
@@ -49,9 +50,27 @@ export class SurveysComponent implements OnInit {
         'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse4.mm.bing.net/th?id=OIP.qE8HSmTXq_mPe8_Tx3c7OAHaEK&pid=Api',
       activate: true,
@@ -67,13 +86,41 @@ export class SurveysComponent implements OnInit {
     }, {
       id: 'id survey2',
       title: 'title2',
-      creator: {id: 'id user', username: 'user namevery lonnnnnnnnnng'},
-      description: 'description',
+      creator: {
+        id: 'id user',
+        username: 'user name',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      },
+      slogan: 'mon petit slogan un peu long ggggggggg par cequerfz ouefzeiun zfer vuibnezfze ',
+      description: 'description qui est vraiment pas long du tout v' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user2', username: 'user2'}, {id: 'id user3', username: 'user3'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse3.mm.bing.net/th?id=OIP._7Fk7RukccwyRphGJvZdhgHaHa&pid=Api',
       activate: true,
@@ -89,13 +136,41 @@ export class SurveysComponent implements OnInit {
     }, {
       id: 'id survey2',
       title: 'title2',
-      creator: {id: 'id user', username: 'user name'},
-      description: 'description decreaczpznrpi ijz epij piln,pipij pij,pmj p ij pzepejfzepij pizej,pmj knlknlk:n;, pomjpmj, . pimkj, ',
+      creator: {
+        id: 'id user',
+        username: 'user name',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      },
+      slogan: 'mon petit slogan un peu long ggggggggg par cequerfz ouefzeiun zfer vuibnezfze ',
+      description: 'description qui est vraiment pas long du tout v' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user2', username: 'user2'}, {id: 'id user3', username: 'user3'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse3.mm.bing.net/th?id=OIP._7Fk7RukccwyRphGJvZdhgHaHa&pid=Api',
       activate: true,
@@ -111,13 +186,41 @@ export class SurveysComponent implements OnInit {
     }, {
       id: 'id survey2',
       title: 'title2',
-      creator: {id: 'id user', username: 'user name'},
-      description: 'description',
+      creator: {
+        id: 'id user',
+        username: 'user name',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      },
+      slogan: 'mon petit slogan un peu long ggggggggg par cequerfz ouefzeiun zfer vuibnezfze ',
+      description: 'description qui est vraiment pas long du tout v' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user2', username: 'user2'}, {id: 'id user3', username: 'user3'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse3.mm.bing.net/th?id=OIP._7Fk7RukccwyRphGJvZdhgHaHa&pid=Api',
       activate: true,
@@ -133,13 +236,41 @@ export class SurveysComponent implements OnInit {
     }, {
       id: 'id survey2',
       title: 'title2',
-      creator: {id: 'id user', username: 'user name'},
-      description: 'description',
+      creator: {
+        id: 'id user',
+        username: 'user name',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      },
+      slogan: 'mon petit slogan un peu long ggggggggg par cequerfz ouefzeiun zfer vuibnezfze ',
+      description: 'description qui est vraiment pas long du tout v' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user2', username: 'user2'}, {id: 'id user3', username: 'user3'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse3.mm.bing.net/th?id=OIP._7Fk7RukccwyRphGJvZdhgHaHa&pid=Api',
       activate: true,
@@ -155,13 +286,41 @@ export class SurveysComponent implements OnInit {
     }, {
       id: 'id survey2',
       title: 'title2',
-      creator: {id: 'id user', username: 'user name'},
-      description: 'description',
+      creator: {
+        id: 'id user',
+        username: 'user name',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      },
+      slogan: 'mon petit slogan un peu long ggggggggg par cequerfz ouefzeiun zfer vuibnezfze ',
+      description: 'description qui est vraiment pas long du tout v' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user2', username: 'user2'}, {id: 'id user3', username: 'user3'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse3.mm.bing.net/th?id=OIP._7Fk7RukccwyRphGJvZdhgHaHa&pid=Api',
       activate: true,
@@ -177,13 +336,41 @@ export class SurveysComponent implements OnInit {
     }, {
       id: 'id survey2',
       title: 'title2',
-      creator: {id: 'id user', username: 'user name'},
-      description: 'description',
+      creator: {
+        id: 'id user',
+        username: 'user name',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      },
+      slogan: 'mon petit slogan un peu long ggggggggg par cequerfz ouefzeiun zfer vuibnezfze ',
+      description: 'description qui est vraiment pas long du tout v' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user2', username: 'user2'}, {id: 'id user3', username: 'user3'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse3.mm.bing.net/th?id=OIP._7Fk7RukccwyRphGJvZdhgHaHa&pid=Api',
       activate: true,
@@ -199,13 +386,41 @@ export class SurveysComponent implements OnInit {
     }, {
       id: 'id survey2',
       title: 'title2',
-      creator: {id: 'id user', username: 'user name'},
-      description: 'description',
+      creator: {
+        id: 'id user',
+        username: 'user name',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      },
+      slogan: 'mon petit slogan un peu long ggggggggg par cequerfz ouefzeiun zfer vuibnezfze ',
+      description: 'description qui est vraiment pas long du tout v' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'oir pas du tout tout toutoutoutoutou ze zgrgrgzr zgdbdder z' +
+        'zeze zrze ze zrg ze oljkzefoil zerzeolirjnze zesoil kznefilzen ez lzekjrf',
       theme: 'string',
       tags: ['tag', 'tag2'],
-      members: [{id: 'id user2', username: 'user2'}, {id: 'id user3', username: 'user3'}],
-      editors: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
-      admins: [{id: 'id user1', username: 'user1'}, {id: 'id user2', username: 'user2'}],
+      members: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      editors: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
+      admins: [{
+        id: 'id user1', username: 'user1',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }, {
+        id: 'id user2', username: 'user2',
+        photo: 'https://tse1.mm.bing.net/th?id=OIP.o_BpuBv1QSN1S0kMWQjZawHaHa&pid=Api'
+      }],
       candidatesIds: ['id1', 'id2'],
       images: 'https://tse3.mm.bing.net/th?id=OIP._7Fk7RukccwyRphGJvZdhgHaHa&pid=Api',
       activate: true,
