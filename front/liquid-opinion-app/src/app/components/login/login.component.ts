0b45803c06837e6f55e06b1345eefd262a0b9d4e
niko <diablade@hotmail.com>
2021-02-12 16:47:26 +0100

    front draft login message

--- front/liquid-opinion-app/src/app/components/login/login.component.ts
+++ front/liquid-opinion-app/src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../services/auth.service';
-import {Router} from '@angular/router';
+import {first} from 'rxjs/operators';
+
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
 	selector: 'app-login',
@@ -13,19 +15,15 @@ export class LoginComponent implements OnInit {
 	authFormRegister: FormGroup;
 	authFormLogin: FormGroup;
 	isSubmitted = false;
-	regex = '^[a-zA-Z0-9]{8,}$';
-	bestRegex = '^[a-zA-Z0-9!"#\\$%&\'\\(\\)\\*\\+,-\\.\\/:;<=>\\?@[\\]\\^_`\\{\\|}~]{8,}$';
+	currentUrl: String;
 
-	email = new FormControl('', [Validators.required, Validators.email]);
-	password = new FormControl('', [Validators.required, Validators.pattern(this.regex)]);
+	regexUsername = '^[a-zA-Z0-9]{4,}$';
+	regexPass = '^[a-zA-Z0-9]{8,}$';
+	bestRegexPass = '^[a-zA-Z0-9!"#\\$%&\'\\(\\)\\*\\+,-\\.\\/:;<=>\\?@[\\]\\^_`\\{\\|}~]{8,}$';
 
-	constructor(
-		// private authService: AuthService, public location: Location, private router: Router, private formBuilder: FormBuilder
-	) {
-	}
-
-	get formControls() {
-		return this.authFormRegister.controls;
+	constructor(private authService: AuthService,
+				private route: ActivatedRoute,
+				private router: Router, private fb: FormBuilder) {
 	}
 
 	getErrorEmail() {
@@ -42,29 +40,62 @@ export class LoginComponent implements OnInit {
 		return this.email.hasError('email') ? 'password must be : min 8 Char, lowercase, uppercase letters and numbers' : '';
 	}
 
+	getErrorPseudo() {
+		if (this.username.hasError('required')) {
+			return 'You must enter a value';
+		}
+		return this.username.hasError('pseudo') ? 'only letters, numbers' : '';
+	}
+
 	ngOnInit(): void {
-		// this.authForm = this.formBuilder.group({
-		// 	email: ['', Validators.required],
-		// 	password: ['', Validators.required]
-		// });
+		// get return url from route parameters or default to '/'
+		this.currentUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+		this.authFormRegister = this.fb.group({
+			email: ['', Validators.required, Validators.email],
+			username: ['', Validators.required, Validators.email],
+			password: ['', Validators.required, Validators.pattern(this.regexPass)],
+		});
+		this.authFormLogin = this.fb.group({
+			email: ['', Validators.required, Validators.email],
+			password: ['', Validators.required, Validators.pattern(this.regexPass)]
+		});
 	}
 
 	register() {
-		// this.isSubmitted = true;
-		// if (this.authForm.invalid) {
-		// 	return;
-		// }
-		// this.authService.login(this.authForm.password,this.);
-		// this.router.navigate(this.location.pathname);
-		// window.location.reload();
+		this.isSubmitted = true;
+		if (this.authFormRegister.invalid) {
+			return;
+		}
+		this.authService.login(this.authFormRegister.get('email'), this.authFormRegister.get('username'), this.authFormRegister.get('password'))
+			.pipe()
+			.subscribe({
+				next: () => {
+					this.router.navigate([this.currentUrl]);
+				},
+				error: error => {
+					// this.error = error;
+					// this.loading = false;
+				}
+			});
 	}
+
 	login() {
-		// this.isSubmitted = true;
-		// if (this.authForm.invalid) {
-		// 	return;
-		// }
-		// this.authService.login(this.authForm.password,this.);
-		// this.router.navigate(this.location.pathname);
-		// window.location.reload();
+		this.isSubmitted = true;
+		if (this.authForm.invalid) {
+			return;
+		}
+		this.authService.login(this.authFormLogin.get('email'), this.authFormLogin.get('password'))
+			.pipe(first())
+			.subscribe({
+				next: () => {
+					this.router.navigate([this.currentUrl]);
+				},
+				error: error => {
+					// this.error = error;
+					// this.loading = false;
+				}
+			});
 	}
+
 }
